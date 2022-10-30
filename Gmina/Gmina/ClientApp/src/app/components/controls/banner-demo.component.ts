81e152f2ebc31247f16e7c5c4f9a727340c60f9f
Jan Dyrduł <j.dyrdul@itmcode.pl>
2022-10-30 14:32:37 +0100

    init

---
+++ Gmina/Gmina/ClientApp/src/app/components/controls/banner-demo.component.ts
@@ -0,0 +1,17 @@
+ 
+ 
+ 
+ 
+
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-banner-demo',
+  templateUrl: './banner-demo.component.html',
+  styleUrls: ['./banner-demo.component.scss']
+})
+export class BannerDemoComponent {
+
+}
---
+++ Gmina/Gmina/DesignTimeDbContextFactory.cs
@@ -0,0 +1,36 @@
+ 
+ 
+ 
+ 
+
+using AutoMapper;
+using DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Gmina
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile("appsettings.Development.json", optional: true)
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            builder.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"], b => b.MigrationsAssembly("Gmina"));
+
+            return new ApplicationDbContext(builder.Options);
+        }
+    }
+}
